32ef847e64ac8545064717be29cf75301a67848b
Alexander Strelchuk <worxvaliev@gmail.com>
2026-08-29 15:08:35 +0500

    Add KafkaProducer

---
+++ game-service/app/infrastructure/kafka/producer.py
@@ -0,0 +1,15 @@
+
+
+class KafkaProducer:
+
+    def __init__(self):
+        ...
+
+    async def start(self):
+        ...
+
+    async def stop(self):
+        ...
+
+    async def senf(self):
+        ...
