09a246686dee5d429ac4842ad3bdef04feebef59
Alexander Valiev <worxvaliev@gmail.com>
2026-09-03 23:17:36 +0300

    Update KafkaProducer

--- game-service/app/api/v1/router.py
+++ game-service/app/api/v1/router.py
@@ -10,10 +10,13 @@ router = APIRouter(prefix="/api/v1")
 
 @router.post(path="/send-event")
 async def send_event(data: EventSchema):
-    await kafka_producer.send(data)
-    return JSONResponse(content={"event_id": str(data.event_id)}, status_code=200)
+    await kafka_producer.send(event=data, topic="game-events")
+    return JSONResponse(
+        content={"event_id": str(data.event_id)},
+        status_code=200
+    )
 
 
-@router.get("/metrics")
+@router.get(path="/metrics")
 async def get_metrics():
     return [await consumer.get_metrics() for consumer in game_consumers]
